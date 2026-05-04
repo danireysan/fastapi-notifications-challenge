8fb6a050335f36c73ffed66847dc5715801e1652
Daniel Reynoso Sanchez <dreynoso@tribalworldwide.gt>
2026-05-03 19:33:30 -0600

    created login usecase

--- core/auth/application/login_usecase.py
+++ core/auth/application/login_usecase.py
@@ -1,2 +1,10 @@
 
 
+from core.application.base_usecase import IUseCase
+from core.auth.application.submit_credentials_command import SubmitCredentialsCommand
+from core.auth.application.submit_credentials_result import SubmitCredentialsResult
+
+
+class LoginUseCase(IUseCase[SubmitCredentialsResult, SubmitCredentialsCommand]):
+    async def call_async(self, params):
+        return await super().call_async(params)
